af21cdc79cfd3beaa68036ec0c36c9b4d3263c34
Claudio Traspadini Oliveira <caltras@gmail.com>
2019-04-03 21:40:57 +0100

    Part2

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -19,5 +19,12 @@ describe('7Shifts Test', ()=>{
             expect(calc.add("1")).toBe(1 as Int);
         });
     });
-    
+    describe('Part 2', ()=>{
+        it('Add "1\\n,2,3" expected 6', ()=>{
+            expect(calc.add("1\n,2,3")).toBe(6 as Int);
+        });
+        it('Add "1,2\\n,4" expected 7', ()=>{
+            expect(calc.add("1,2\n,4")).toBe(7 as Int);
+        });
+    });
 })
